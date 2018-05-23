e598101c118760c86e50baa5b01af31f455b7e46
Swen Kunzel <swenkuenzel@gmail.com>
2018-05-23 11:34:35 +0100

    reviews views and controller ready

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,30 @@
 class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: [ :show]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    Restaurant.create(review_params)
+    redirect_to restaurants_path
+  end
+
+  def show
+
+  end
+
+  private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def review_params
+    params.require(:restaurant).permit(:name,:address,:category,:phone_number)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    resources :restaurants do
-      resources :reviews, only: [ :new, :create ]
+    resources :restaurants, only: [ :index, :create, :new, :show ]  do
+      resources :reviews, only: [  :index, :create, :new, :show ]
   end
 end
