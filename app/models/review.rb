5311d95777ed2cc12cd25118ee07dcdef48568d7
Swen Kunzel <swenkuenzel@gmail.com>
2018-05-23 09:45:34 +0100

    first setup with routes and controllers

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  belongs_to :restaurant
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '123',
+    category: 'chinese'
+
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '456',
+    category: 'french'
+
+  },
+  {
+    name:         'Rest 3',
+    address:      'Address 3',
+    phone_number:  '789',
+    category: 'italian'
+
+  },
+    {
+    name:         'Rest 4',
+    address:      'Address 4',
+    phone_number:  '101112',
+    category: 'japanese'
+
+  },
+    {
+    name:         'Rest 5',
+    address:      'Address 5',
+    phone_number:  '131415',
+    category: 'belgian'
+
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
